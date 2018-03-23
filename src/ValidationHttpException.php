5a77772b34aa2e31411a7069e2b1c55bb398b750
object <lpg525@icloud.com>
2018-03-23 11:37:35 +0000

    去除命名空间的驼峰发

--- src/ValidationHttpException.php
+++ src/ValidationHttpException.php
@@ -9,7 +9,7 @@
 // | Author: Object,半醒的狐狸<2252390865@qq.com>
 // +----------------------------------------------------------------------
 
-namespace ChinaObject\PreRequest;
+namespace Chinaobject\PreRequest;
 
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
